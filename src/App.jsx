e0ee0b38ac577eed902542f30c34ae17a148cca2
rigal34 <94939344+rigal34@users.noreply.github.com>
2026-08-03 17:03:26 +0200

    Ajout de l'llustration d'accueil

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,18 @@ function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header></Header>
-      <main className="flex-1 max-w-4xl mx-auto p-4"></main>
+      <main className="flex-1 max-w-4xl mx-auto p-4">
+        <img
+          src="/accueil.svg"
+          alt="Bienvenue sur TaskManager"
+          style={{
+            display: "block",
+            margin: "auto",
+            width: "100%",
+            maxWidth: "820px",
+          }}
+        />
+      </main>
       <Footer></Footer>
     </div>
   );
---
+++ src/exox/lcompte.js
@@ -0,0 +1,15 @@
+
+
+const croissant = () =>{
+    
+    const tableau  = [17	,13	,31,	3,	46
+    ];
+
+const trier = tableau.sort((a,b) => a - b);
+
+console.log(trier);
+
+};
+
+
+croissant();
